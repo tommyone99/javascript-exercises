dea6ab35db7319c74b80b6016778fe147aa9fd52
Thomas One <tommyone99@gmail.com>
2023-10-18 19:38:50 -0400

    complete requirements for first test of 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(userString) {
+    let reversedString = userString.split("").reverse().join("");
+    return reversedString;
 };
 
 // Do not edit below this line
