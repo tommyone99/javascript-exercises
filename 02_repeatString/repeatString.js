c69a0e5096a5c7fe94d4971c8d01172c5f71c023
Thomas One <tommyone99@gmail.com>
2023-10-18 16:58:42 -0400

    complete all 02_repeatString tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,11 +1,15 @@
 const repeatString = function(string, quantity) {
+    if (quantity < 0) {
+        return "ERROR";
+    }
 
-    let repeatedString ="";
+    let repeatedString = "";
 
     for (let i = 0; i < quantity; i++) {
         repeatedString = repeatedString + string;
     }
     return repeatedString;
+
 };
 
 // Do not edit below this line
