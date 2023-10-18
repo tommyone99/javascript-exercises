7fc58afdf7cdbf3ddc434e08aa477dd02dbf2975
Thomas One <tommyone99@gmail.com>
2023-10-18 16:34:16 -0400

    complete first two tests of repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, quantity) {
 
+    let repeatedString ="";
+
+    for (let i = 0; i < quantity; i++) {
+        repeatedString = repeatedString + string;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
