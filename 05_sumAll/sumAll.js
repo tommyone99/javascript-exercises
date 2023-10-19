d141b65ac632bc141b3146d4aa80b0451682e7c9
Thomas One <tommyone99@gmail.com>
2023-10-19 02:31:09 -0400

    complete first and second requirements for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
 
+    let sum = 0;
+    for (let i = firstNumber; i <= secondNumber; i++) {
+        sum = sum + i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
