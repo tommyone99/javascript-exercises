6a4d4770e217fa86c41842a3187988b2ada302e2
Thomas One <tommyone99@gmail.com>
2023-10-19 02:48:27 -0400

    change code to validate more inputs i.e. criteria 4 for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,24 @@
 const sumAll = function(firstNumber, secondNumber) {
 
     let sum = 0;
-    for (let i = firstNumber; i <= secondNumber; i++) {
-        sum = sum + i;
+
+    if (Number.isInteger(firstNumber) != true || Number.isInteger(secondNumber) != true) {
+        return "ERROR";
     }
+    else if (firstNumber < 0 || secondNumber < 0) {
+        return "ERROR";
+    }
+    else if (firstNumber < secondNumber) {
+        for (let i = firstNumber; i <= secondNumber; i++) {
+            sum = sum + i;
+        }
+    }
+    else if (firstNumber > secondNumber) {
+        for (let i = secondNumber; i <= firstNumber; i++) {
+            sum = sum + i;
+        }
+    }
+
 
     return sum;
 };
