8f049bfeb2f58dcacd32ea1af3a55541f997f623
Thomas One <tommyone99@gmail.com>
2023-10-18 20:27:19 -0400

    complete requirements for first test of 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, elementToRemove) {
 
+    let i = 0;
+
+    for (element of array) {
+        if (element === elementToRemove) {
+            array.splice(i, 1);
+            i--;
+        }
+        i++;
+    }
+    return array;
 };
 
 // Do not edit below this line
