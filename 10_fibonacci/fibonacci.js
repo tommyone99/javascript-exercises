47334f561b317b2565eec3c1cbcd791f2b1a3881
Thomas One <tommyone99@gmail.com>
2023-11-06 00:42:51 -0500

    create fibonacci function with for loop

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let arrFib = [0, 1, 1];
 
+    for (let i = 2; i < n; i++) {
+        arrFib[i + 1] = arrFib[i] + arrFib[i-1];
+    }
+
+    return arrFib.pop();
 };
 
 // Do not edit below this line
