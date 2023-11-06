77c35abed7d93ffb27c604b4d57269220776bdff
Thomas One <tommyone99@gmail.com>
2023-11-06 00:51:29 -0500

    add input validation for negatives and non-numbers

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,7 +1,10 @@
 const fibonacci = function(n) {
     let arrFib = [0, 1, 1];
 
+    if (Number.isInteger(n) != true || n < 0) return "OOPS";
+
     for (let i = 2; i < n; i++) {
+        //next array element is the sum of the last two numbers in the array
         arrFib[i + 1] = arrFib[i] + arrFib[i-1];
     }
 
