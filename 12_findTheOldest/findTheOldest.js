fa9abecdd0f1cc448c310ed2827b8bce2518b5e7
Thomas One <tommyone99@gmail.com>
2023-11-07 13:10:48 -0500

    complete 12_findTheOldest assignment

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    //get current time for current age calculation
+    const date = new Date();
+    
+    people.forEach(function (person) {
 
+        if ("yearOfDeath" in person){
+            person.lifeLived = Number(person.yearOfDeath - person.yearOfBirth);
+        }
+        else  {
+            person.lifeLived = Number(date.getFullYear() - person.yearOfBirth);
+        };
+    });
+
+    people.sort((a, b) => (a.lifeLived > b.lifeLived) ? 1 : -1);
+
+    return people.pop();
+    
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
